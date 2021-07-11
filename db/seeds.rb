0a67f93c493e792c324113a1507ea5075268c765
池田聖示 <ikeda-seiji@ikedakiyoshishimesenoMacBook-Pro-2.local>
2021-07-11 21:45:33 +0900

    add timezone, create init deta

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!(name: 'dummy1', start: '2021-7-27 10:00:00', end: '2020-10-27 11:00:00')
+Event.create!(name: 'dummy2', start: '2021-7-27 10:00:00', end: '2020-10-27 11:00:00')
+Event.create!(name: 'dummy3', start: '2021-7-27 10:00:00', end: '2020-10-27 11:00:00')
+Event.create!(name: 'dummy4', start: '2021-7-27 10:00:00', end: '2020-10-27 11:00:00')
